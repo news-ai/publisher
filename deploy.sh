c0abb4e1e3c4ad7186d77b01e5303865354a6af6
Abhi Agarwal <me@abhiagarwal.com>
2016-03-21 00:01:57 -0400

    Switching to v0.0.2 and adding cache-control to deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
 npm run build
 gsutil rsync -d -r build gs://publisher.newsai.org/
 gsutil -m acl set -R -a public-read gs://publisher.newsai.org/
+gsutil web set -m index.html -e 404.html gs://publisher.newsai.org/
+
+gsutil setmeta -h "Content-Type:text/html" -h "Cache-Control:public, max-age=3600" -h "Content-Disposition" gs://publisher.newsai.org/index.html
+
+gsutil setmeta -h "Content-Type:application/javascript" -h "Cache-Control:public, max-age=3600" -h "Content-Disposition" gs://publisher.newsai.org/serviceworker.js
+
+gsutil setmeta -h "Content-Type:application/javascript" -h "Cache-Control:public, max-age=3600" -h "Content-Disposition" gs://publisher.newsai.org/js/bundle.js
+
+gsutil setmeta -h "Content-Type:text/css" -h "Cache-Control:public, max-age=3600" -h "Content-Disposition" gs://publisher.newsai.org/css/main.css
+
