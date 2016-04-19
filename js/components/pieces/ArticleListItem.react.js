98a435b305bd7329e9c68e4c5b050d7156397121
Julie Pan <ycp217@nyu.edu>
2016-04-19 00:32:43 -0400

    add link

--- js/components/pieces/ArticleListItem.react.js
+++ js/components/pieces/ArticleListItem.react.js
@@ -1,16 +1,20 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-let moment = require('moment-timezone');
+const moment = require('moment-timezone');
 
-function ArticleListItem({id, name, url, summary, added_at, authors, publisher, entity_scores}) {
+function ArticleListItem({id, name, url, summary, added_at, publisher, entity_scores}) {
   const timestring = moment(added_at).tz('America/New_York').format('MMM D, YYYY hh:mm A');
-            /*<i
+            /* <i
             className='fa fa-star fa-2x pull-right'
             style={{
               color: 'gray'
             }}
             ariaHidden='true'
-            ></i>*/
+            ></i>
+          <div className='article-authors'>
+          {authors.map((author, i) => <Link to={'/authors/' + author.id}><span key={i}>{author.name}</span></Link>)}
+          </div>
+            */
   return (
     <div className='row article-body' key={id}>
       <div className='twelve columns'>
@@ -20,21 +24,19 @@ function ArticleListItem({id, name, url, summary, added_at, authors, publisher, 
           <div className='article-publisher'>
             <Link to={'/publishers/' + publisher.id}><span>{publisher.name}</span></Link>
           </div>
-            {entity_scores.length >= 3 ? (
+            { entity_scores.length >= 3 ? (
             <div className='article-top3-entities'>
-              {entity_scores.map((entity, i) => (i < 3) ?
+              { entity_scores.map((entity, i) => i < 3 ?
               <Link to={'/entities/' + entity_scores[i].entity.id}><div>{entity_scores[i].entity.name}</div></Link>
               : null)}
             </div>
               ) : null}
-          <div className='article-authors'>
-          {authors.map((author, i) => <span key={i}><Link to={'/authors/' + author.id}>{author.name} </Link></span>)}
-          </div>
           <div className='article-bulletpoints'>
           <span>
           {summary}
           </span>
           <div>
+            <a style={{fontSize: '0.8em'}} href={url}>{url}</a>
             <span className='pull-right article-list-item-date'>{timestring}</span>
           </div>
           </div>
